17e7ce750991f1c4a7677ee3d8ee981e327434d6
Dora Xiao <dlxiao@LAPTOP-DUF7JNPL.localdomain>
2022-10-31 23:24:45 -0400

    change status codes to 400

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -27,27 +27,27 @@ def validate_inputs(url):
     assert (response.get_data() == 0 or response.get_data() == 1)
 
     failed_response1 = client.get(url, json = {"absences": 4, "health": 3})
-    assert failed_response1.status_code == 200
+    assert failed_response1.status_code == 400
     assert failed_response1.get_data() == "Invalid query: Arguments are missing"
 
     failed_response2 = client.get(url, json = {"age": 4, "health": 3})
-    assert failed_response2.status_code == 200
+    assert failed_response2.status_code == 400
     assert failed_response2.get_data() == "Invalid query: Arguments are missing"
 
     failed_response3 = client.get(url, json = {"age": "Eighteen", "absences": 2, "health": 3})
-    assert failed_response3.status_code == 200
+    assert failed_response3.status_code == 400
     assert failed_response3.get_data() == "Invalid query: Arguments have invalid types or ranges"
 
     failed_response4 = client.get(url, json = {"age": -5, "absences": 2, "health": 3})
-    assert failed_response4.status_code == 200
+    assert failed_response4.status_code == 400
     assert failed_response4.get_data() == "Invalid query: Arguments have invalid types or ranges"
 
     failed_response5 = client.get(url, json = {"age": 17, "absences": -1, "health": 3})
-    assert failed_response5.status_code == 200
+    assert failed_response5.status_code == 400
     assert failed_response5.get_data() == "Invalid query: Arguments have invalid types or ranges"
 
     failed_response6 = client.get(url, json = {"age": 17, "absences": 24, "health": 0})
-    assert failed_response6.status_code == 200
+    assert failed_response6.status_code == 400
     assert failed_response6.get_data() == "Invalid query: Arguments have invalid types or ranges"
 
 def test_predict_route():
